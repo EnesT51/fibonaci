9e2771a39ecd86c012f3b7a11cced79d2f9ca44a
EnesT51 <99040669@mydavinci.nl>
2023-02-16 14:36:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Fibonaci;
+public class Fibonaci
+{
+    public static void Main()
+    {
+        List<int> list = new List<int>();
+        for (int i = 0; i < 20; i++)
+        {
+            if (i < 2) { list.Add(i); }
+            else
+            {
+                int x = list[i - 1] + list[i - 2];
+                list.Add(x);
+            }
+        }
+        list.ForEach(Console.WriteLine);
+    }
+}
